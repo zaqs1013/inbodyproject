8c6d6a30381f57f889a9d121a6cb89748ca8d1a0
jihoon629 <149461835+jihoon629@users.noreply.github.com>
2025-01-06 17:15:54 +0900

    Add files via upload

--- record/recordBody.php
+++ record/recordBody.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>recoredBod</title>
     <link rel="stylesheet" href="https://unpkg.com/mvp.css" />
-    <link rel="stylesheet" href="./css/censter.css" />
+  <link rel="stylesheet" href="../static/css/center.css" />
+
 </head>
 
 <?php
