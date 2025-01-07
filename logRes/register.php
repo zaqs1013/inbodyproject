7daeb4f0307f8476054953f37ca01e2363fcc321
PARKSEONAH251 <miss.parkseonah0501@gmail.com>
2025-01-07 21:15:15 +0900

    regiset_UI_change

--- logRes/register.php
+++ logRes/register.php
@@ -4,23 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Do Hyeon font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Do+Hyeon&family=Oswald:wght@200..700&display=swap" rel="stylesheet">
     <title>register</title>
-    <link rel="stylesheet" href="https://unpkg.com/mvp.css" />
-    <link rel="stylesheet" href="./css/censter.css" />
-    </head>
-
+    <link rel="stylesheet" href="../static/css/register.css" />
+    <link rel="stylesheet" href="../static/css/register.css" />
+</head>
 <body>
     <form action="./registerOk.php" method="post" id="from" enctype="multipart/form-data">
-        <h1>회원 가입</h1>
-
+        <h1>JOIN</h1>
 
         <label for="name">이름</label>
         <input type="text" id="name" name="name" required title="이름을 입력해주세요.">
 
         <div>
             <label for="sex">성별</label>
-            남 <input type="radio" id="male" name="sex" value="1">
-            여 <input type="radio" id="female" name="sex" value="2">
+            <text id="man">남</text>
+            <input type="radio" id="male" name="sex" value="1">
+            <text id="wonman">여</text>
+            <input type="radio" id="female" name="sex" value="2">
         </div>
 
 
@@ -32,12 +36,15 @@
 
         <div>
             <label for="phone">전화 번호</label>
-            010 - <input type="tel" id="phNum1" name="phNum1" maxlength="4"   pattern="\d{4}" placeholder="1234" required title="전화번호를 입력해주세요."> - <input type="tel" id="phNum2"
-                name="phNum2" maxlength="4"  pattern="\d{4}" placeholder="1234" required title="전화번호를 입력해주세요.">
+            &nbsp;&nbsp;010&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;
+            <input type="tel" id="phNum1" name="phNum1" maxlength="4"   pattern="\d{4}" placeholder="1234" required title="전화번호를 입력해주세요."> 
+            &nbsp;-&nbsp;
+            <input type="tel" id="phNum2"
+            name="phNum2" maxlength="4"  pattern="\d{4}" placeholder="1234" required title="전화번호를 입력해주세요.">
         </div>
 
         <label for="ID">아이디</label>
-        <input type="text" id="ID" name="ID" required title="아이디를 입력해주세요.">
+        <input type="text" id="ID" name="ID" required title="아이디를 입력해주세요."><br>
         <label for="PW">비밀번호</label>
         <input type="password" id="PW" name="PW" required title="비밀번호를 입력해주세요.">
 
@@ -46,5 +53,4 @@
         <button>가입</button>
     </form>
 </body>
-
 </html>
